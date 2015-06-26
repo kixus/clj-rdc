3acced1630a592b1891aee9d30d411bbb3eed3bd
Rajesh Krishnan <raj.dev.github@pub.dom.st>
2015-06-26 11:05:03 -0700

    Updated project.clj with :scm info

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject kixus/reduce           "0.1.0"
+(defproject kixus/reduce           "0.1.1"
     :description                    "Generalized multi-dimensional reduce functions in Clojure."
     :url                            "https://github.com/kixus/reduce-clj"
+    :scm            {   :name       "git" 
+                        :url        "https://github.com/midpeter444/go-lightly"
+                    }
     :license        {   :name       "BSD 3-Clause License"
                         :url        "http://opensource.org/licenses/BSD-3-Clause"
                     }
-    :dependencies   [   [org.clojure/clojure "1.6.0"]
+    :dependencies   [               [org.clojure/clojure    "1.6.0"]
                     ]
     :profiles       {   :dev        [   ;; :dependencies   [
                                         ;;                 ]
@@ -16,6 +19,8 @@
     :repositories   {   "sonatype"  "https://oss.sonatype.org/content/repositories/releases/"
                     }
     :target-path    "out/tgt"
-    :source-paths   ["src" "tst"]
+    :source-paths   ["src"]
+    :test-paths     ["tst"]
     :resource-paths ["res"]
+    :jvm-opts       ["-Xmx4g"]
 )
